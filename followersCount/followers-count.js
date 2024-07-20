b52d47c1b846596af0f37c439d8aed1d68a52b9a
Astra <joanatlfsilva@gmail.com>
2024-07-20 08:22:56 -0400

    practice resert button

--- followersCount/followers-count.js
+++ followersCount/followers-count.js
@@ -2,22 +2,25 @@ let count = 0;
 console.log(count);
 
 function increaseCount() {
-    console.log("increaseCount was called");
     count++;
     displayCount();
     checkCountValue();
 }
 
 function displayCount() {
-    console.log("displayCount was called");
     document.getElementById("countDisplay").innerHTML = count;
 }
 
 function checkCountValue() {
-    console.log("checkCountValue was called");
     if (count === 10) {
         alert("Your Insta post gained 10 followers! Congrats!");
     } else if (count === 20) {
         alert("Your Insta post gained 20 followers! Congrats!");
     }
+}
+
+function resetCount() {
+    count = 0
+    document.getElementById("countDisplay").innerHTML = count;
+    alert("Followers count has been reset.")
 }
